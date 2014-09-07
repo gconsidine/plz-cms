2c1368b4713e306ef6ebe4cc11630529ef409dbb
Greg Considine <greg@greg-considine.com>
2014-09-06 20:25:26 -0700

    Clean up index.js

--- index.js
+++ index.js
@@ -1,17 +1,17 @@
-var Index = function (options) {
+module.exports = function (options) {
   'use strict';
 
-  if(options === 'undefined') {
+  if(!options) {
     throw new Error([
       'Configuration options must be supplied when requiring plz-cms',
-      'Example: `var plz = require("plz-cms")(options);`',
-      '`options` reference: https://github.com/gconsidine/plz-cms'
+      'Example: var plz = require("plz-cms")(options);',
+      'Reference: https://github.com/gconsidine/plz-cms'
     ].join('\n'));
+    
+    return false;
   }
 
   var Hub = require('./app/Hub');
 
   return Hub.configure(options);
 };
-
-module.exports = Index;
