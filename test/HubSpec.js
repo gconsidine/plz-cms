3f066aceabfe397326bfa25fb46ba9a8b55bfdbf
Greg Considine <greg@greg-considine.com>
2014-09-09 22:02:37 -0700

    Add tests for Hub.js

--- test/HubSpec.js
+++ test/HubSpec.js
@@ -1,13 +1,83 @@
 (function () {
   'use strict';
 
-  require('should');
+  var Should = require('should'),
+      Hub = require('../app/Hub');
+  
+  var _validOptions = {
+    modules: {
+    },
+    database: {
+      default: {
+        uri: process.env.PLZ_DATABASE_DEFAULT
+      }
+    },
+    mailer: {
+      default: {
+        service: 'Gmail',
+        address: process.env.PLZ_MAILER_DEFAULT_ADDRESS,
+        password: process.env.PLZ_MAILER_ACTIVATION_PASSWORD
+      }
+    }
+  };
+
+  var _invalidOptions = {
+    modules: {
+    }, 
+    database: {
+      notDefault: 'mongo://'
+    },
+    mailer: {
+    }
+  };
 
   describe('Hub | configure()', function () {
 
-    it('should accept a valid options object', function () {
+    it('should not accpet undefined options', function (done) {
+      try {
+        Hub.configure();
+        Should.fail();
+      } catch(error) {
+        done();
+      }
+    });
+
+    it('should not accept malformed options', function (done) {
+      try {
+        Hub.configure(_invalidOptions); 
+        Should.fail();
+      } catch(error) {
+        done();
+      }
+    });
+
+    it('should accept valid options', function (done) {
+      try {
+        Hub.configure(_validOptions);  
+        Should.pass();
+      } catch(error) {
+        done();
+      }
     });
 
   });
 
+  describe('Hub | Hub-specific generated API', function () {
+   it('is a placeholder', function () {
+     true.should.be.true;
+   });
+  });
+
+  describe('Hub | plz-admin-specific generated API', function () {
+    it('is a placeholder', function () {
+     true.should.be.true;
+    });
+  });
+
+  describe('Hub | Combined APIs', function () {
+    it('is a placeholder', function () {
+     true.should.be.true;
+    });
+  });
+ 
 }());
--- test/IndexSpec.js
+++ test/IndexSpec.js
@@ -1,13 +1,13 @@
 (function () {
   'use strict';
 
-  var should = require('should');
+  var Should = require('should');
 
   describe('plz-cms | require("plz-cms")(); without options', function () {
     it('should throw an error', function (done) {
       try {
         require('../index')();
-        should.fail();
+        Should.fail();
       } catch(error) {
         done();
       }
