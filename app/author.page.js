6d424f1504ce3b2d3c72391f92de02728c4144d3
Greg Considine <greg@greg-considine.com>
2014-10-19 19:12:31 -0700

    Make small (mostly) cosmetic changes

--- app/author.page.js
+++ app/author.page.js
@@ -1,11 +1,10 @@
 /**
  * Contains CRUD actions that can be performed on a page from the author 
- * component
-
+ * component.
+ *
  * @memberof author
  * @namespace author.page
  */
-
 var AuthorPage = function (plz) {
   'use strict';
   
@@ -22,7 +21,8 @@ var AuthorPage = function (plz) {
 
   /**
   * Creates a page with given options and inserts it into the database, 
-  * if allowed
+  * if allowed.
+  *
   * @memberof author.page
   * @param {object} options
   * @param {string} options.userName - Used to check permissions
@@ -32,14 +32,11 @@ var AuthorPage = function (plz) {
   * @param {string} options.contentType - plain test / HTML / markdown
   * @param {string} options.content - String containing actual contents of page
   * @param {string} options.createdAt - Current timestamp upon successful 
-  *                                     insertion into database
+  * insertion into database
   * @param {string} options.modifiedAt - Current timestamp upon successful 
-  *                                      insertion into database
+  * insertion into database
   * @param {string} options.status -- draft/pending review/published
-  * @param {access} callback
-  * callback parameters: errorFlag -- true/false
-  *                      result -- result from DB insert upon success or 
-  *                                description string upon error
+  * @param {page} callback
   *  (perhaps this should be simply errorDescription || null)
   */
   plz.create.page = function (options, callback) {
@@ -54,8 +51,7 @@ var AuthorPage = function (plz) {
       // var query = {collection:plz.config.admin.collection,
       //              searchKey:email}
       //              entry:options}
-      //plz.create.dbentry(query, function(error, result){});
-
+      // plz.create.dbentry(query, function(error, result){});
       plz.get.database(function (error, database) {
         _page = database.collection(_collectionName);
 
@@ -80,35 +76,30 @@ var AuthorPage = function (plz) {
           options.modifiedAt = currentTimestamp;
           options.status = 'created';
 
-          // insert into database
           _page.insertOne(options, function (error, result) {
             if(error) {
               callback(true, 'Insert failed');
               return;
             }
 
-            // invoke callback with result
             callback(false, result);
           });
         });
-	  });
+  	  });
     });
   };
 
   /**
   * Publishes a page matching title in options, making it publicly
   * available for reading/routing
+  *
   * @memberof author.page
   * @param {object} options
   * @param {string} options.userName - Used to check permissions
   * @param {string} options.pageTitle - Unique id used in page lookup
-  * callback parameters: errorFlag -- true/false
-  *                      result -- result from DB insert upon success or 
-  *                                description string upon error
+  * @param {page} callback
   */
   plz.publish.page = function (options, callback) {
-    //TODO: check permissions via plz.restritct.user
-    //  check required fields
     if(typeof options.userName !== 'string' ||
        typeof options.pageTitle !== 'string') {
       callback(true, 'Required field not present in options');
@@ -119,72 +110,63 @@ var AuthorPage = function (plz) {
       _page = database.collection(_collectionName);
 
       // TODO: support search by _id
-      //       If saving multiple revisions, get latest or check revisionNumber
+      // If saving multiple revisions, get latest or check revisionNumber
       var criteria = {pageTitle: options.pageTitle};
       var update = {$set:{visibility: "public", status: "published"}};
 
-      // update database entry 
       _page.updateOne(criteria, update, function(error, result){
 
-		// invoke callback with result or error
         if(error) {
           callback(true, 'Edit failed: ' + error);
           return;
         }
 
-        if(!result || 
-           result.matchedCount === 0 || 
-           result.modifiedCount === 0){
+        if(!result || result.matchedCount === 0 || result.modifiedCount === 0) {
           callback(true, 'Could not find page with title ' + options.pageTitle);
           return;
         }
 
         callback(false, result);
-
       });
     });
   };
 
   /**
   * Fetches a page object matching the title specified in options
+  *
   * @memberof author.page
   * @param {object} options
   * @param {string} options.userName - Used to check permissions
   * @param {string} options.pageTitle - Unique id used in page lookup
-  * callback parameters: errorFlag -- true/false
-  *                      result -- result from DB fetch upon success or 
-  *                                description string upon error
+  * @param {page} callback
   */
   plz.get.page = function (options, callback) {
-    //TODO: check permissions via plz.restritct.user
-    // check required fields
     if(typeof options.pageTitle !== 'string') {
       callback(true, 'Required field not present in options');
       return;
     }
 
-    // fetch database entry 
     plz.get.database(function (error, database) {
       _page = database.collection(_collectionName);
-      _page.findOne({pageTitle: options.pageTitle}, function(error, result){
+
+      _page.findOne({pageTitle: options.pageTitle}, function(error, result) {
         var message;
-        if (error)
-        {
+
+        if (error) {
           message = 'Failed to find entry matching ' + options.pageTitle;
           message += ' in ' + plz.config.author.page.collection;
+
           callback(true, message);
           return;
-        }
-        else
-        {
-          if (!result || 
-              result.matchedCount === 0 || 
+        } else {
+          if (!result || result.matchedCount === 0 || 
               result.modifiedCount === 0){
             message = 'Could not find page with title ' + options.pageTitle;
+
             callback(true, message);
             return;
           }
-          // invoke callback with result
+
           callback(false, result);
         }
       });
@@ -194,17 +176,15 @@ var AuthorPage = function (plz) {
   /**
   * Modifies the content of a page if it exists based on the criteria options 
   * passed as the first argument.
+  *
   * @memberof author.page
   * @param {object} options
   * @param {string} options.userName - Used to check permissions
   * @param {string} options.pageTitle - Unique id used in page lookup
   * @param {string} options.content - Replacement content for page
-  * callback parameters: errorFlag -- true/false
-  *                      result -- result from DB edit upon success or 
-  *                                description string upon error
+  * @param {page} callback
   */
   plz.edit.page = function (options, callback) {
-    //  check required fields
     if(typeof options.userName !== 'string' ||
        typeof options.pageTitle !== 'string' ||
        typeof options.content !== 'string') {
@@ -221,10 +201,14 @@ var AuthorPage = function (plz) {
 
       var currentTimestamp = new Date().getTime() / 1000;
       var criteria = {pageTitle: options.pageTitle};
-      var update = {$set:{content: options.content, 
-                          modifiedAt: currentTimestamp}};
-      _page.findOneAndUpdate(criteria, update,
-        function (error, result) {
+      var update = {
+        $set:{
+          content: options.content, 
+          modifiedAt: currentTimestamp
+        }
+      };
+
+      _page.findOneAndUpdate(criteria, update, function (error, result) {
         if(error) {
           callback(true, 'Edit failed');
           return;
@@ -243,16 +227,14 @@ var AuthorPage = function (plz) {
   /**
   * Deletes a page if it exists based on the criteria options passed as the
   * first argument.
+  *
   * @memberof author.page
   * @param {object} options
   * @param {string} options.userName - Used to check permissions
   * @param {string} options.pageTitle - Unique id used in page lookup
-  * callback parameters: errorFlag -- true/false
-  *                      result -- result from DB remove upon success or 
-  *                                description string upon error
+  * @param {page} callback
   */
   plz.remove.page = function (options, callback) {
-    //  check required fields
     if(typeof options.userName !== 'string' ||
        typeof options.pageTitle !== 'string') {
       callback(true, 'Required field not present in options');
@@ -299,3 +281,10 @@ var AuthorPage = function (plz) {
 };
 
 module.exports = AuthorPage;
+
+/**
+* @callback page
+* @param {boolean} error - Indicating success/failure of the call
+* @param {string|object} result - result from DB call upon success or a
+* description string upon error
+*/
