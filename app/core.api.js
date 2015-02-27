d47cdcc1204486d62751145562b8d1aa8c3b7948
Greg Considine <greg@greg-considine.com>
2015-02-26 20:23:04 -0800

    Separate user features out from master

--- app/core.api.js
+++ app/core.api.js
@@ -13,11 +13,6 @@ var CoreApi = function (config) {
     require('./admin.account')(plz);
   }
 
-  if(plz.config.modules.author) {
-    require('./author.page')(plz);
-    require('./author.post')(plz);
-  }
-
   return plz;
 };
 
