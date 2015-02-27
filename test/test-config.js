fb8a2fd6531f03ff7b18d599ee73c88fa0b9598b
Greg Considine <greg@greg-considine.com>
2015-02-26 20:29:29 -0800

    Isolate author module from user

--- test/test-config.js
+++ test/test-config.js
@@ -55,58 +55,6 @@ var TestConfig = function () {
     }
   };
 
-  var validAdminConfig = {
-    modules: {
-      admin: true
-    },
-    database: {
-      default: {
-        uri: MONGO_URI
-      }
-    },
-    mailer: {
-      default: {
-        service: '',
-        address: 'sender@example.com',
-        password: ''
-      }
-    },
-    admin: {
-      collection: 'user',
-      roles: {
-        admin: true,
-        user: true
-      },
-      required: {
-        name: 'string',
-        email: 'email',
-        password: 'password',
-        createdAt: 'number',
-        modifiedAt: 'number',
-        lastLogin: 'number',
-        status: 'string'
-      }
-    }
-  };
-
-  var invalidAdminConfig = {
-    modules: {
-      admin: true
-    },
-    database: {
-      default: {
-        uri: MONGO_URI
-      }
-    },
-    mailer: {
-      default: {
-        service: '',
-        address: 'sender@example.com',
-        password: '' 
-      }
-    }
-  };
-
   var validAuthorConfig = {
     modules: {
       admin: false,
@@ -234,31 +182,9 @@ var TestConfig = function () {
     status: 'draft'
   };
 
-  var validUser = {
-    name: 'greg',
-    email: 'sender@example.com',
-    password: 'someFakePass0',
-    createdAt: 3134999944,
-    modifiedAt: 3134999945,
-    lastLogin: 0,
-    status: 'active',
-    role: 'admin'
-  };
-
-  var invalidUser = {
-    name: 'greg',
-    email: 'sender@exmaple.com',
-    password: 'someFakePass0',
-    createdAt: 3134999944,
-    modifiedAt: 3134999945,
-    role: 'admin'
-  };
-  
   return {
     validCoreConfig: validCoreConfig,
     invalidCoreConfig: invalidCoreConfig,
-    validAdminConfig: validAdminConfig,
-    invalidAdminConfig: invalidAdminConfig,
     validAuthorConfig: validAuthorConfig,
     invalidAuthorConfig: invalidAuthorConfig,
     validPage: validPage,
@@ -267,8 +193,6 @@ var TestConfig = function () {
     validPost: validPost,
     anotherValidPost: anotherValidPost,
     invalidPost: invalidPost,
-    validUser: validUser,
-    invalidUser: invalidUser,
     invalidDatabase: invalidDatabase
   };
 };
